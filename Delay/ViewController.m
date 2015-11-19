bc0e0f1311ad7151087e6f27aca529da7b0cd48a
Katelyn Findlay <kfindlay@bluebeam.com>
2015-11-19 13:01:24 -0800

    init commit

--- Delay/ViewController.m
+++ Delay/ViewController.m
@@ -10,18 +10,33 @@
 
 @interface ViewController ()
 
+@property (nonatomic) UITableView* mediaTableView;
+@property (nonatomic) NSArray *mediaStrings;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.mediaTableView = [UITableView new];
+    self.mediaStrings = @[@"Twitter", @"Mail", @"iMessage", @""];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MediaCell"];
+    cell.textLabel.text = self.mediaStrings[indexPath.row];
+    return cell;
 }
 
 @end
